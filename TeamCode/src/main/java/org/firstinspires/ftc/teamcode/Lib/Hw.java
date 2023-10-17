30a60d11d71201062d9357a46d387f6e2e271129
DAuto <darryl.greathouse@gmail.com>
2023-10-17 14:04:16 -0400

    Arranged Hw for Control and Expansion hub strings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/Hw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/Hw.java
@@ -7,16 +7,19 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.IMU;
 
 public class Hw {
+    // Control Hub
     public static String s_fl = "l"; // Drive Left Motor
     public static String s_fr = "r"; // Drive Right Motor
     public static String s_b = "b";  // Drive Back Motor
     public static String s_SH = "s"; // Shoulder Motor
-    public static String s_clawRotate = "crm";
     public static String s_clawSR = "csr";
-    public static String s_Drone = "d";
-    public static String s_forearm = "f";
     public static String s_ledTop = "lt";
     public static String s_ledBot = "lb";
+    // Expansion hub
+    public static String s_clawRotate = "crm";
+    public static String s_Drone = "d";
+    public static String s_forearm = "f";
+
     public static GamepadEx s_gpOperator, s_gpDriver;
     public static IMU s_imu;
     CommandOpMode m_opMode;
