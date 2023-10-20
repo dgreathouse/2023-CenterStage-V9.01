47d28d1e18f467d60f97ee79d72d9882e7831fa3
DAuto <darryl.greathouse@gmail.com>
2023-10-19 20:31:36 -0400

    Added Team Prop Drive Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Drive/AutoDriveToTeamProp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Drive/AutoDriveToTeamProp.java
@@ -3,41 +3,67 @@ package org.firstinspires.ftc.teamcode.Commands.Drive;
 import com.arcrobotics.ftclib.command.CommandBase;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.util.Timing;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Lib.TeamPropLocation;
+import org.firstinspires.ftc.teamcode.Lib.k;
 import org.firstinspires.ftc.teamcode.Subsystems.DriveSubsystem;
 
-/** Drive the robot at a speed, angle for a certain time. Also rotate the robot to an angle while driving
-   Since the robot drives at a velocity the only changing variable to get a consistent distance is
-   the battery which changes the time it takes to go from stop to full velocity. We hope this change
-   will not affect the accuracy to much. There is no PID on distance and the coast time will need
-   to be considered.
+import java.util.concurrent.TimeUnit;
+
+/** Drive to Team Prop Location. The robot starts in the center.
+ * The Global variable k.ARM.TeamPropLocation is set by a locator before this command.
+ * The location will determine the angle to drive and rotate.
+ * Those angles are stored here instead of the k.ARM location.
+ *
  */
 public class AutoDriveToTeamProp extends CommandBase {
     CommandOpMode m_opMode;
     DriveSubsystem m_drive;
-    double m_angle;
-    double m_robotAngle;
-    double m_timeOut;
-    double m_speed;
+    double m_angle = 0;
+    double m_robotAngle = 0;
+    int m_timeOut = 1;
+    double m_speed = 0.5;
 
     PIDController rotPID = new PIDController(.01,0,0);
-    ElapsedTime m_elapsedTimer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+    Timing.Timer m_elapsedTimer;
+    //ElapsedTime m_elapsedTimer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
 
     public AutoDriveToTeamProp(CommandOpMode _opMode, DriveSubsystem _drive) {
         m_opMode = _opMode;
         m_drive = _drive;
-//        m_angle = _angle;
-//        m_speed = _speed;
-//        m_robotAngle = _robotAngle;
-//        m_timeOut = _timeOut;
+
     }
 
     @Override
     public void initialize(){
         rotPID.reset();
         m_drive.resetMotors();
-        m_elapsedTimer.reset();
+
+        m_speed = 0.5;
+        switch (k.ARM.TeamPropLoc){
+            case CENTER:
+                m_angle = 0;
+                m_robotAngle = 0;
+                m_timeOut = 1;
+            break;
+            case LEFT:
+                m_angle = 1;
+                m_robotAngle = 1;
+                m_timeOut = 1;
+                break;
+            case RIGHT:
+                m_angle = 2;
+                m_robotAngle = 2;
+                m_timeOut = 1;
+                break;
+            default:
+                break;
+
+        }
+        m_elapsedTimer =  new Timing.Timer(m_timeOut, TimeUnit.MILLISECONDS);
+        m_elapsedTimer.start();
     }
     @Override
     public void execute(){
@@ -48,7 +74,8 @@ public class AutoDriveToTeamProp extends CommandBase {
     }
     @Override
     public boolean isFinished(){
-        if(m_elapsedTimer.seconds() > m_timeOut){
+        if(m_elapsedTimer.done()){
+            m_drive.disableMotors();
             return true;
         }
         return false;
