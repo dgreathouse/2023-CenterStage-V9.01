3ba0f6457a99c651ce58c0c62d81a823a06028c4
DAuto <darryl.greathouse@gmail.com>
2023-10-19 20:36:52 -0400

    Added Drive to backdrop code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Drive/AutoDriveToBackdrop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Drive/AutoDriveToBackdrop.java
@@ -3,9 +3,13 @@ package org.firstinspires.ftc.teamcode.Commands.Drive;
 import com.arcrobotics.ftclib.command.CommandBase;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.util.Timing;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.Lib.k;
 import org.firstinspires.ftc.teamcode.Subsystems.DriveSubsystem;
+
+import java.util.concurrent.TimeUnit;
 
 /** Drive the robot at a speed, angle for a certain time. Also rotate the robot to an angle while driving
    Since the robot drives at a velocity the only changing variable to get a consistent distance is
@@ -16,28 +20,48 @@ import org.firstinspires.ftc.teamcode.Subsystems.DriveSubsystem;
 public class AutoDriveToBackdrop extends CommandBase {
     CommandOpMode m_opMode;
     DriveSubsystem m_drive;
-    double m_angle;
-    double m_robotAngle;
-    double m_timeOut;
-    double m_speed;
+    double m_angle = 0;
+    double m_robotAngle = 0;
+    int m_timeOut = 1;
+    double m_speed = 0.5;
 
     PIDController rotPID = new PIDController(.01,0,0);
-    ElapsedTime m_elapsedTimer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+    Timing.Timer m_elapsedTimer;
 
     public AutoDriveToBackdrop(CommandOpMode _opMode, DriveSubsystem _drive) {
         m_opMode = _opMode;
         m_drive = _drive;
-//        m_angle = _angle;
-//        m_speed = _speed;
-//        m_robotAngle = _robotAngle;
-//        m_timeOut = _timeOut;
     }
 
     @Override
     public void initialize(){
         rotPID.reset();
         m_drive.resetMotors();
-        m_elapsedTimer.reset();
+
+        m_speed = 0.5;
+        switch (k.ARM.TeamPropLoc){
+            case CENTER:
+                m_angle = 0;
+                m_robotAngle = 0;
+                m_timeOut = 1;
+                break;
+            case LEFT:
+                m_angle = 1;
+                m_robotAngle = 1;
+                m_timeOut = 1;
+                break;
+            case RIGHT:
+                m_angle = 2;
+                m_robotAngle = 2;
+                m_timeOut = 1;
+                break;
+            default:
+                break;
+
+        }
+        m_elapsedTimer =  new Timing.Timer(m_timeOut, TimeUnit.MILLISECONDS);
+        m_elapsedTimer.start();
+
     }
     @Override
     public void execute(){
@@ -48,7 +72,8 @@ public class AutoDriveToBackdrop extends CommandBase {
     }
     @Override
     public boolean isFinished(){
-        if(m_elapsedTimer.seconds() > m_timeOut){
+        if(m_elapsedTimer.done()){
+            m_drive.disableMotors();
             return true;
         }
         return false;
