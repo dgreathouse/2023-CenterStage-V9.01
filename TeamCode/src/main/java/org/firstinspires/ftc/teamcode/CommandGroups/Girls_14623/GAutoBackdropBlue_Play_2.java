0ec555fa08cfd1360fada5f349687a9708b429eb
DAuto <darryl.greathouse@gmail.com>
2023-10-24 17:13:09 -0400

    Updated to new buttons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/Girls_14623/GAutoBackdropBlue_Play_2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/Girls_14623/GAutoBackdropBlue_Play_2.java
@@ -4,6 +4,7 @@ import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 
+import org.firstinspires.ftc.teamcode.Commands.Arm.ArmGetTeamPropLocation;
 import org.firstinspires.ftc.teamcode.Commands.AutoStopOpModeCommand;
 import org.firstinspires.ftc.teamcode.Lib.GlobalData;
 import org.firstinspires.ftc.teamcode.Lib.TeamColor;
@@ -15,6 +16,7 @@ public class GAutoBackdropBlue_Play_2 extends SequentialCommandGroup {
     public GAutoBackdropBlue_Play_2(CommandOpMode _opMode, DriveSubsystem _drive, ArmSubsystem _arm) {
         GlobalData.s_teamColor = TeamColor.BLUE;
         addCommands(
+                new ArmGetTeamPropLocation(_opMode, _arm),
             new ParallelCommandGroup(
 
             ),
