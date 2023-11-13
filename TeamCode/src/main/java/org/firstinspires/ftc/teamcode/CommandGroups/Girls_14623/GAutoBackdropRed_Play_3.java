6905ba6548ef04112e4b4bde596284479292d448
dgreathouse <darryl.greathouse@gmail.com>
2023-11-13 13:57:02 -0500

    Created AutoRotateToTeamProp to make the robot rotate instead of drive and rotate. This should be faster.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/Girls_14623/GAutoBackdropRed_Play_3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/Girls_14623/GAutoBackdropRed_Play_3.java
@@ -16,6 +16,7 @@ import org.firstinspires.ftc.teamcode.Commands.Drive.AutoDriveToBackdropAprilTag
 import org.firstinspires.ftc.teamcode.Commands.Drive.AutoDriveToPark;
 import org.firstinspires.ftc.teamcode.Commands.Drive.AutoDriveToTeamProp;
 import org.firstinspires.ftc.teamcode.Commands.Drive.AutoRotateRobot;
+import org.firstinspires.ftc.teamcode.Commands.Drive.AutoRotateToTeamProp;
 import org.firstinspires.ftc.teamcode.Lib.ArmData;
 import org.firstinspires.ftc.teamcode.Lib.ArmPos;
 import org.firstinspires.ftc.teamcode.Lib.AutoFieldLocation;
@@ -41,9 +42,9 @@ public class GAutoBackdropRed_Play_3 extends SequentialCommandGroup {
                 new AutoRotateRobot(_opMode,_drive, -50,0.25,3000),                                    // Rotate to other team prop location
                 new ArmGetTeamPropLocation(_opMode, _arm, TeamPropLocation.RIGHT),                                           // Read team prop location (right)
                 new ArmGetTeamPropLocation(_opMode, _arm, TeamPropLocation.LEFT),                                            // Read team prop location (left), just calculates the final position
-                new AutoRotateRobot(_opMode,_drive, 0,0.25,3000),                                      // Rotate robot straight
+                //new AutoRotateRobot(_opMode,_drive, 0,0.25,3000),                                      // Rotate robot straight
                 new ArmAutoGotoPosition(_opMode, _arm, armData.getArmSetAngle(ArmPos.STACK_5),0,0),   // Put arm in position to drop pixel
-                new AutoDriveToTeamProp(_opMode,_drive),                                                                     // Drive to team prop method that just rotates to a location
+                new AutoRotateToTeamProp(_opMode,_drive),                                                                     // Drive to team prop method that just rotates to a location
                 new ArmAutoGotoPosition(_opMode, _arm, armData.getArmSetAngle(ArmPos.STACK_5),0,100), // Extend arm to spike mark
                 new AutoDelayCommand(_opMode,1250),                                                                  // Delay long enough for arm to reach spike mark
                 new ClawRotateFingers(_opMode, _claw, _claw.getClawReleaseLowerAngle()),                                     // Release the lower pixel only
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Drive/AutoRotateToTeamProp.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode.Commands.Drive;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.util.Timing;
+
+import org.firstinspires.ftc.teamcode.Lib.GlobalData;
+import org.firstinspires.ftc.teamcode.Lib.TeamColor;
+import org.firstinspires.ftc.teamcode.Lib.k;
+import org.firstinspires.ftc.teamcode.Subsystems.AutoDriveSubsystem;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Drive to Team Prop Location. The robot starts in the center.
+ * The Global variable k.ARM.TeamPropLocation is set by a locator before this command.
+ * The location will determine the angle to drive and rotate.
+ * Those angles are stored here instead of the k.ARM location.
+ */
+public class AutoRotateToTeamProp extends CommandBase {
+    CommandOpMode m_opMode;
+    AutoDriveSubsystem m_drive;
+
+    double m_robotAngle = 0;
+    int m_timeOut = 2000;
+    double m_speed = 0.4;
+
+    PIDController rotPID;
+    Timing.Timer m_timer;
+
+    public AutoRotateToTeamProp(CommandOpMode _opMode, AutoDriveSubsystem _drive) {
+        m_opMode = _opMode;
+        m_drive = _drive;
+    }
+
+    @Override
+    public void initialize() {
+        rotPID = new PIDController(k.DRIVE.Rot_P, k.DRIVE.Rot_I, 0);
+        rotPID.setTolerance(1.0);
+        rotPID.reset();
+
+        switch (GlobalData.TeamPropLocation) {
+            case CENTER:
+            case NONE:
+                m_robotAngle = GlobalData.TeamColor == TeamColor.BLUE ? 17.5 : -17.5;
+                break;
+            case LEFT:
+                m_robotAngle = 45;
+                break;
+            case RIGHT:
+                m_robotAngle = -45;
+                break;
+
+        }
+
+        m_timer = new Timing.Timer(m_timeOut, TimeUnit.MILLISECONDS);
+        m_timer.start();
+    }
+
+    @Override
+    public void execute() {
+
+        double rot = -rotPID.calculate(m_drive.getRobotAngle(), m_robotAngle);
+        m_drive.drivePolar(0, m_speed, rot);
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        if(m_timer.done() || rotPID.atSetPoint()){
+            m_drive.disableMotors();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void end(boolean _interrupted) {
+        m_drive.disableMotors();
+    }
+}
