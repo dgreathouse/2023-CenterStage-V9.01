1971ce25fe38ccc288f68953cae264abc479e58f
dgreathouse <darryl.greathouse@gmail.com>
2023-12-13 14:37:51 -0500

    Updated angles in Wing Wall get two

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/Girls_14623/Common/WingWallToBackdrop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/Girls_14623/Common/WingWallToBackdrop.java
@@ -20,13 +20,13 @@ public class WingWallToBackdrop extends SequentialCommandGroup {
         double sign = GlobalData.MATCH.AutoTeamColor == TeamColor.BLUE ? 1.0 : -1;
         addCommands(
 
-                new AutoRotateRobot(_opMode,_drive, -90 * sign,0.25,3),                                // Rotate to go under truss
-                new AutoDriveTimeVel(_opMode, _drive, -90 * sign, 0.8, -90 * sign,2.05),                      // Drive under truss
+                new AutoRotateRobot(_opMode,_drive, -90 * sign,0.25,3),                         // Rotate to go under truss
+                new AutoDriveTimeVel(_opMode, _drive, -90 * sign, 0.8, -90 * sign,2.05),        // Drive under truss
                 new InstantCommand(() -> _arm.setArmData(42,30,0)),                             // Raise the arm to the backdrop
                 new AutoDelayCommand(_opMode, 0.0),                                             // TODO: Add a delay if teams need it
                 new AutoDriveToBackdropFromWing(_opMode,_drive),                                // Drive to the backdrop
                 new InstantCommand(_claw::setClawReleaseUpperAngle),                            // Release the upper pixel
-                new AutoDriveTimeVel(_opMode, _drive, 90 * sign, 0.5, 90 * sign,0.6),                         // Drive back a little
+                new AutoDriveTimeVel(_opMode, _drive, 90 * sign, 0.5, 90 * sign,0.6),           // Drive back a little
                 new InstantCommand(() -> _arm.setArmData(0,0,0)),                               // Lower the arm
                 new AutoDelayCommand(_opMode, 10),                                              // Delay so the arm comes down
                 new InstantCommand(_opMode::requestOpModeStop)                                  // This must be the last line of every command list
